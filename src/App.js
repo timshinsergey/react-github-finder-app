b514c65abaad996490e3c7887ccc60ac0226deaa
Sergey <33506866+timshinsergey@users.noreply.github.com>
2022-01-04 20:00:34 +0400

    creating User page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Navbar from './components/layout/Navbar'
 import Footer from './components/layout/Footer'
 import Alert from './components/layout/Alert'
 import Home from './pages/Home'
+import User from './pages/User'
 import About from './pages/About'
 import NotFound from './pages/NotFound'
 import { GithubProvider } from './context/github/GithubContext'
@@ -21,6 +22,7 @@ function App() {
 							<Routes>
 								<Route path='/' element={<Home />} />
 								<Route path='/about' element={<About />} />
+								<Route path='/user/:login' element={<User />} />
 								<Route path='/notfound' element={<NotFound />} />
 								<Route path='/*' element={<NotFound />} />
 							</Routes>
